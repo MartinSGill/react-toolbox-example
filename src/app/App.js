d3cb30559197503785d3f428fa0d0fa813fa7da9
Oleg Stepura <github@oleg.stepura.com>
2017-01-23 17:20:21 +0200

    Concrete example to test NavDrawer permanentAt property (from here: http://react-toolbox.com/#/components/layout)

--- src/app/App.js
+++ src/app/App.js
@@ -1,16 +1,54 @@
 import React from 'react';
-import PurpleAppBar from './PurpleAppBar.js';      // AppBar with simple overrides
-import SuccessButton from './SuccessButton.js';    // A button with complex overrides
-import { Button } from 'react-toolbox/lib/button'; // Bundled component import
+import { AppBar, Checkbox, IconButton } from 'react-toolbox';
+import { Layout, NavDrawer, Panel, Sidebar } from 'react-toolbox';
 
-const App = () => (
-  <div>
-    <PurpleAppBar />
-    <section style={{ padding: 20 }}>
-      <SuccessButton label='Success' primary raised />
-      <Button label='Primary Button' primary />
-    </section>
-  </div>
-);
+class App extends React.Component {
+  state = {
+    drawerActive: false,
+    drawerPinned: false,
+    sidebarPinned: false
+  };
+
+  toggleDrawerActive = () => {
+    this.setState({ drawerActive: !this.state.drawerActive });
+  };
+
+  toggleDrawerPinned = () => {
+    this.setState({ drawerPinned: !this.state.drawerPinned });
+  };
+
+  toggleSidebar = () => {
+    this.setState({ sidebarPinned: !this.state.sidebarPinned });
+  };
+
+  render() {
+    return (
+      <Layout>
+        <NavDrawer active={this.state.drawerActive}
+                   pinned={this.state.drawerPinned} permanentAt='lg'
+                   onOverlayClick={ this.toggleDrawerActive }>
+          <p>
+            Navigation, account switcher, etc. go here.
+          </p>
+        </NavDrawer>
+        <Panel>
+          <AppBar leftIcon='menu' onLeftIconClick={ this.toggleDrawerActive } />
+          <div style={{ flex: 1, overflowY: 'auto', padding: '1.8rem' }}>
+            <h1>Main Content</h1>
+            <p>Main content goes here.</p>
+            <Checkbox label='Pin drawer' checked={this.state.drawerPinned} onChange={this.toggleDrawerPinned} />
+            <Checkbox label='Show sidebar' checked={this.state.sidebarPinned} onChange={this.toggleSidebar} />
+          </div>
+        </Panel>
+        <Sidebar pinned={ this.state.sidebarPinned } width={ 5 }>
+          <div><IconButton icon='close' onClick={ this.toggleSidebar }/></div>
+          <div style={{ flex: 1 }}>
+            <p>Supplemental content goes here.</p>
+          </div>
+        </Sidebar>
+      </Layout>
+    );
+  }
+}
 
 export default App;
